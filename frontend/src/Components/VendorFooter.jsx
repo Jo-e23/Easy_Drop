8bae82eca3b80f0c0553539ffaaf776ff4845630
ANU-ASOKAN <anuasokankollam@gmail.com>
2025-01-09 16:11:25 +0530

    Vendor dashboard

---
+++ frontend/src/Components/VendorFooter.jsx
@@ -0,0 +1,6 @@
+export default function VendorFooter(){
+    return(
+        <div>
+        </div>
+    )
+}
---
+++ frontend/src/Components/VendorHeader.jsx
@@ -0,0 +1,7 @@
+export default function VendorHeader(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
