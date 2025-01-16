729a8832a47769a188e6490b1f671371315f5fe3
Monai Abraham <abrahammonai35@gmail.com>
2025-01-16 16:47:20 +0530

    category api created

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,11 +10,30 @@ const userSchema = new Schema({
     username: String, 
     phone: Number,
     email: String,
+    address:String,
+    businessName:String,
+
     password:String
    
   });
 
   const userModel =mongoose.model("users",userSchema);
+
+  const vendorSchema = new Schema({
+    id:ObjectId,
+    createdOn:Date,
+    username: String, 
+    phone: Number,
+    email: String,
+    address:String,
+    categoryname:String,
+    businessName:String,
+    password:String
+   
+  });
+
+  const vendorModel =mongoose.model("vendors",vendorSchema);
+
 
 //Read Customer API
 
@@ -49,5 +68,29 @@ router.get('/login', function(req, res, next) {
     res.send("Error",e);
   })
 });
+
+
+// read category
+router.get('/category', function(req, res, next) {
+  vendorModel.find().then((vendors)=>{
+res.send(vendors);
+  }).catch((e)=>{
+    res.send("Error",e);
+  })
+});
+
+ // create Category name
+ router.post('/createCategory', function(req, res, next) {
+  vendorModel.create(req.body).then((vendor)=>{
+    res.send(vendor);
+  }).catch((e)=>{
+    res.send("Error",e);
+  })
+});
+
+// get vendors list
+
+
+
 
 module.exports = router;
